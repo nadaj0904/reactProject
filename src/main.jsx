f2307ff63fac2b4ae71107bb959191ffc26e726a
Namchang Lee <namchang@example.com>
2025-08-10 15:54:23 +0900

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import "./styles/index_kong.css";
+// import "./index.css";
+// import "./styles/index_kong.css";
 
-//import App1 from "./App1.jsx"; /** 공미준 사이트 테스트 */
-import App from "./App.jsx"; /** 라이프 홈페이지 샘플코드 */
+import App1 from "./App1.jsx"; /** 공미준 사이트 테스트 */
+// import App from "./App.jsx"; /** 라이프 홈페이지 샘플코드 */
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <App1 />
   </BrowserRouter>
 );
